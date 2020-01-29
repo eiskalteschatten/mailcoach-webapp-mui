901b2c372cb055d838295d2f5332d5df1ba89e68
Alex Seifert <alex@alexseifert.com>
2020-01-29 11:52:08 +0100

    Fix tests

---
+++ server/jest.config.js
@@ -0,0 +1,17 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/', '/config/test.js', '/dist/', '/src/db/migrations'],
+  testPathIgnorePatterns: ['/node_modules/', '/config/test.js', '/dist/', '/src/db/migrations'],
+  collectCoverage: true,
+  coverageDirectory: './test-coverage',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' }),
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json'
+    }
+  }
+};
