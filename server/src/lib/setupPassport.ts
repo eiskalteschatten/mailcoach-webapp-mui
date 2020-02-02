700a8348f5a8452318b00bcdd4944d88e2bb63ec
Alex <alex@alexseifert.com>
2020-02-02 11:50:39 +0100

    Set up Passport for using access tokens and refresh tokens

--- server/src/lib/setupPassport.ts
+++ server/src/lib/setupPassport.ts
@@ -3,6 +3,9 @@ import passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 
 import logger from '@mc/lib/logger';
+import User from '@mc/modules/auth/models/User';
+import UserSession from '@mc/modules/auth/models/UserSession';
+import UserService from '@mc/modules/auth/services/UserService';
 
 const jwtRefreshTokenConfig = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -19,11 +22,23 @@ const jwtAccessTokenConfig = {
 export default async function (): Promise<void> {
   passport.use('jwt-refresh-token', new JwtStrategy(jwtRefreshTokenConfig, async (req: Request, jwtPayload: any, done: Function): Promise<void> => {
     try {
-      // const userId = jwtPayload.id;
+      const userId = jwtPayload.id;
 
-      // Check if the user id exists as a key in the DB with the refresh token
+      const userSession = await UserSession.findOne({
+        where: {
+          fkUser: userId
+        },
+        include: [{
+          model: User,
+          as: 'user'
+        }]
+      });
 
-      done(null, false);
+      if (!userSession || !userSession.user) {
+        done(null, false);
+      }
+
+      done(null, userSession.user);
     }
     catch(error) {
       logger.error(error);
@@ -33,9 +48,15 @@ export default async function (): Promise<void> {
 
   passport.use('jwt', new JwtStrategy(jwtAccessTokenConfig, async (req: Request, jwtPayload: any, done: Function): Promise<void> => {
     try {
-      // const userId = jwtPayload.id;
+      const userService = new UserService();
+      const canLogin = await userService.jwtLogin(jwtPayload.id);
+      const user = userService.getUser();
 
-      done(null, false);
+      if (!canLogin || !user) {
+        return done(null, false);
+      }
+
+      return done(null, user);
     }
     catch(error) {
       logger.error(error);
@@ -43,13 +64,15 @@ export default async function (): Promise<void> {
     }
   }));
 
-  passport.serializeUser((id: number, done: Function): void => {
-    done(null, id);
+  passport.serializeUser((user: User, done: Function): void => {
+    done(null, user.id);
   });
 
   passport.deserializeUser(async (id: string | number, done: Function): Promise<void> => {
     try {
-      done(null, id);
+      const userService = new UserService();
+      const user = await userService.setUser(id);
+      done(null, user);
     }
     catch(error) {
       logger.error(error);
