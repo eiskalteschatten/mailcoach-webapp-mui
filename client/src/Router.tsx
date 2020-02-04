df7ad94e977a18ae2986dd9450238cb2a5b5ceaf
Alex <alex@alexseifert.com>
2020-02-04 17:49:29 +0100

    Fix linting

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,8 +1,7 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Switch, Route, Redirect, withRouter, RouteComponentProps } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-import { IntlContext } from './intl/IntlContext';
 import { State } from './store';
 
 import RouteWithLayout from './components/layout/wrappers/RouteWithLayout';
