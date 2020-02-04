ae8c02c87c58362c13b4b91a3b00955531375892
Alex <alex@alexseifert.com>
2020-02-04 16:46:23 +0100

    Start the layout for the manage account page

--- client/src/pages/user/ManageAccount.tsx
+++ client/src/pages/user/ManageAccount.tsx
@@ -1,17 +1,48 @@
 import React from 'react';
-// import { compose } from 'recompose';
+import { useSelector } from 'react-redux';
 
-// import withToolbar from '../components/layout/wrappers/withToolbar';
-// import MainToolbar from '../components/toolbars/MainToolbar';
+import {
+  makeStyles,
+  Theme,
+  createStyles,
+  Container,
+  Grid,
+  Paper,
+  Button
+} from '@material-ui/core';
+
+import { State, dispatch } from '../../store';
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  paper: {
+    padding: theme.spacing(2)
+  }
+}));
 
 const ManageAccount: React.FC = () => {
-  return (<div>
-    manage account
-  </div>);
+  const classes = useStyles();
+  const user = useSelector((state: State) => state.user.user);
+
+  return (<Container>
+    <Grid
+      container
+      spacing={2}
+      justify='center'
+      alignItems='center'
+    >
+      <Grid item xs={12} md={6}>
+        <Paper className={classes.paper}>
+          test
+        </Paper>
+      </Grid>
+
+      <Grid item xs={12} md={6}>
+        <Paper className={classes.paper}>
+          test
+        </Paper>
+      </Grid>
+    </Grid>
+  </Container>);
 }
 
 export default ManageAccount;
-
-// export default compose(
-//   withToolbar(MainToolbar)
-// )(Dashboard);
