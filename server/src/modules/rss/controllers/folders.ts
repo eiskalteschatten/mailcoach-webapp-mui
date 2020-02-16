37fa856b708471924b8bf08f63fad0cb0344a5e8
Alex <alex@alexseifert.com>
2020-02-16 20:13:14 +0100

    Finish the RSS folders controller

--- server/src/modules/rss/controllers/folders.ts
+++ server/src/modules/rss/controllers/folders.ts
@@ -6,6 +6,8 @@ import { HttpError } from '@mc/lib/Error';
 
 import Folder from '../models/Folder';
 
+import { serialize, deserializeModelCreateUpdate } from '../serializer/folders';
+
 class FoldersController extends AbstractController {
   constructor() {
     super();
@@ -15,9 +17,32 @@ class FoldersController extends AbstractController {
   private initilizeRoutes(): void {
     this.router.get('/', this.getAllFolders);
     this.router.post('/', this.createFolder);
-    this.router.patch('/:id', this.updateFolder);
+    this.router.put('/:id', this.updateFolder);
     this.router.delete('/:id', this.deleteFolder);
   }
+
+  /**
+   * @api {get} /api/rss/folders Get All Folders
+   * @apiName GetAllFolders
+   * @apiGroup RSS
+   * @apiVersion 1.0.0
+   *
+   * @apiHeaderExample {json} Header-Example:
+   *  {
+   *    "Authorization": "Bearer accessToken"
+   *  }
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *  HTTP/1.1 200 OK
+   *  {
+   *    "folders": [
+   *      {
+   *        "id": 1,
+   *        "name": ""
+   *      }
+   *    ]
+   *  }
+   */
 
   private async getAllFolders(req: Request, res: Response): Promise<void> {
     try {
@@ -27,36 +52,118 @@ class FoldersController extends AbstractController {
         ]
       });
 
-      res.json({ folders });
+      res.json({
+        folders: folders.map(serialize)
+      });
     }
     catch(error) {
       returnError(error as HttpError, req, res);
     }
   }
+
+  /**
+   * @api {post} /api/rss/folders Create a Folder
+   * @apiName CreateAFolder
+   * @apiGroup RSS
+   * @apiVersion 1.0.0
+   *
+   * @apiParam {string} name The folder's name.
+   *
+   * @apiParamExample {json} Request-Example:
+   *  {
+   *    "name": ""
+   *  }
+   *
+   * @apiHeaderExample {json} Header-Example:
+   *  {
+   *    "Authorization": "Bearer accessToken"
+   *  }
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *  HTTP/1.1 200 OK
+   *  {
+   *    "folder": {
+   *      "id": 1,
+   *      "name": ""
+   *    }
+   *  }
+   */
 
   private async createFolder(req: Request, res: Response): Promise<void> {
     try {
-      const folder = await Folder.create(req.body);
+      const deserialized = deserializeModelCreateUpdate(req.body);
+      const folder = await Folder.create(deserialized);
 
-      res.json({ folder });
+      res.json({
+        folder: serialize(folder)
+      });
     }
     catch(error) {
       returnError(error as HttpError, req, res);
     }
   }
+
+  /**
+   * @api {put} /api/rss/folders/:id Update a Folder
+   * @apiName UpdateAFolder
+   * @apiGroup RSS
+   * @apiVersion 1.0.0
+   *
+   * @apiParam {number} id The folder's ID.
+   * @apiParam {string} [name] The folder's name.
+   *
+   * @apiParamExample {json} Request-Example:
+   *  {
+   *    "name": ""
+   *  }
+   *
+   * @apiHeaderExample {json} Header-Example:
+   *  {
+   *    "Authorization": "Bearer accessToken"
+   *  }
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *  HTTP/1.1 200 OK
+   *  {
+   *    "folder": {
+   *      "id": 1,
+   *      "name": ""
+   *    }
+   *  }
+   */
 
   private async updateFolder(req: Request, res: Response): Promise<void> {
     try {
+      const deserialized = deserializeModelCreateUpdate(req.body);
       const folder = await Folder.findByPk(req.params.id);
 
-      await folder.update(req.body);
+      await folder.update(deserialized);
 
-      res.json({ folder });
+      res.json({
+        folder: serialize(folder)
+      });
     }
     catch(error) {
       returnError(error as HttpError, req, res);
     }
   }
+
+  /**
+   * @api {delete} /api/rss/folders/:id Delete a Folder
+   * @apiName DeleteAFolder
+   * @apiGroup RSS
+   * @apiVersion 1.0.0
+   *
+   * @apiParam {number} id The folder's ID.
+   *
+   * @apiHeaderExample {json} Header-Example:
+   *  {
+   *    "Authorization": "Bearer accessToken"
+   *  }
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *  HTTP/1.1 204 No Content
+   */
 
   private async deleteFolder(req: Request, res: Response): Promise<void> {
     try {
