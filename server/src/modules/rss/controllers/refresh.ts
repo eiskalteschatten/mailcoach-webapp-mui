4cf0377a90c9423f7c0fc21566e9f72fc44f8290
Alex <alex@alexseifert.com>
2020-02-16 20:20:47 +0100

    Finish the RSS feed refresh controller

--- server/src/modules/rss/controllers/refresh.ts
+++ server/src/modules/rss/controllers/refresh.ts
@@ -5,6 +5,7 @@ import AbstractController from '@mc/modules/AbstractController';
 import { HttpError } from '@mc/lib/Error';
 
 import { refreshAllFeeds, refreshForSingleFeed } from '../helpers/feedsHelper';
+import { serialize as serializeArticles } from '../serializer/articles';
 
 class RefreshController extends AbstractController {
   constructor() {
@@ -17,15 +18,97 @@ class RefreshController extends AbstractController {
     this.router.post('/:feedId', this.refreshSingleFeed);
   }
 
+  /**
+   * @api {post} /api/rss/refresh Refresh All Feeds
+   * @apiName RefreshAllFeeds
+   * @apiGroup RSS
+   * @apiVersion 1.0.0
+   *
+   * @apiHeaderExample {json} Header-Example:
+   *  {
+   *    "Authorization": "Bearer accessToken"
+   *  }
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *  HTTP/1.1 200 OK
+   *  {
+   *    "articles": [
+   *      {
+   *        "id": 1,
+   *        "title": "",
+   *        "link": "",
+   *        "pubDate": "",
+   *        "creator": "",
+   *        "contentSnippet": "",
+   *        "content": "",
+   *        "guid": "",
+   *        "read": false,
+   *        "markedAsReadAt": "",
+   *        "feed": {
+   *          "id": 1,
+   *          "name": "",
+   *          "feedUrl": "",
+   *          "link": "",
+   *          "icon": "",
+   *          "fkFolder": 1
+   *        }
+   *      }
+   *    ]
+   *  }
+   */
+
   private async refreshAllFeeds(req: Request, res: Response): Promise<void> {
     try {
       const articles = await refreshAllFeeds();
-      res.json({ articles });
+      res.json({
+        articles: articles.map(serializeArticles)
+      });
     }
     catch(error) {
       returnError(error as HttpError, req, res);
     }
   }
+
+  /**
+   * @api {post} /api/rss/refresh/:feedId Refresh A Feed
+   * @apiName RefreshAFeed
+   * @apiGroup RSS
+   * @apiVersion 1.0.0
+   *
+   * @apiParam {number} feedId The feed's ID
+   *
+   * @apiHeaderExample {json} Header-Example:
+   *  {
+   *    "Authorization": "Bearer accessToken"
+   *  }
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *  HTTP/1.1 200 OK
+   *  {
+   *    "articles": [
+   *      {
+   *        "id": 1,
+   *        "title": "",
+   *        "link": "",
+   *        "pubDate": "",
+   *        "creator": "",
+   *        "contentSnippet": "",
+   *        "content": "",
+   *        "guid": "",
+   *        "read": false,
+   *        "markedAsReadAt": "",
+   *        "feed": {
+   *          "id": 1,
+   *          "name": "",
+   *          "feedUrl": "",
+   *          "link": "",
+   *          "icon": "",
+   *          "fkFolder": 1
+   *        }
+   *      }
+   *    ]
+   *  }
+   */
 
   private async refreshSingleFeed(req: Request, res: Response): Promise<void> {
     try {
@@ -33,7 +116,7 @@ class RefreshController extends AbstractController {
       const refreshed = await refreshForSingleFeed(feedId);
 
       res.json({
-        articles: refreshed.articles
+        articles: refreshed.articles.map(serializeArticles)
       });
     }
     catch(error) {
