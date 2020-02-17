acdeeedb0ab176c6811b9cb76577858bb09a53f8
Alex <alex@alexseifert.com>
2020-02-17 21:20:42 +0100

    Create unit tests for the RSS folder actions

---
+++ client/src/store/actions/rss/folderActions.spec.ts
@@ -0,0 +1,44 @@
+import { MockStore } from 'redux-mock-store';
+import nock from 'nock';
+
+import {
+  folderSetAll,
+  folderGetAll
+} from './folderActions';
+
+import mockStore from '../../../lib/tests/mockStore';
+
+describe('RSS Folder Actions', () => {
+  test('Setting all folders works', async () => {
+    const localStore: MockStore = mockStore();
+    await localStore.dispatch(folderSetAll([]));
+    const actions = localStore.getActions();
+    expect(actions[0]).toEqual({
+      type: 'FOLDER_SET_ALL',
+      folders: []
+    });
+  });
+
+  test('Getting all folders works', async () => {
+    nock('http://localhost')
+      .get('/api/rss/folders')
+      .reply(200, {
+        folders: []
+      });
+
+    const localStore: MockStore = mockStore();
+    await localStore.dispatch(folderGetAll() as any);
+    const actions = localStore.getActions();
+
+    expect(actions[0]).toEqual({type: 'APP_START_LOADING'});
+    expect(actions[1]).toEqual({
+      type: 'APP_SET_FORM_ERROR',
+      error: ''
+    });
+    expect(actions[2]).toEqual({
+      type: 'FOLDER_SET_ALL',
+      folders: []
+    });
+    expect(actions[3]).toEqual({type: 'APP_STOP_LOADING'});
+  });
+});
